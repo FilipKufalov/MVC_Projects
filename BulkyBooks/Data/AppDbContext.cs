36b260168e30c8ddfa143ab05fabda7d11110ae4
FilipKufalov <filip.kufalov@hotmail.com>
2022-11-11 08:50:10 +0100

    Added Category + Created Database

---
+++ BulkyBooks/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BulkyBooks.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBooks.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
